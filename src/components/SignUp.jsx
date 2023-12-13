9970364cda3c3424937e80985f94d9682a944d1a
Taiwo AKerele <davcodes24@gmail.com>
2023-12-13 13:50:08 +0100

    workin on authentication

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,20 +1,70 @@
-import React from 'react';
-import loginImage from './image/sign.svg';
+import React from "react";
+import { useState } from "react";
+import loginImage from "./image/sign.svg";
 const SignUp = () => {
-    return (
-        <div className='signComponents'>
-            <img src={loginImage} alt='login-image' />
-            <form>
-                <input type='text' placeholder='Full Name' />
-                <input type="email" placeholder='your email' />
-                <input type="password" name="" id="" placeholder='create password' />
-                <input type="password" name="" id="" placeholder='confirm password' />
-                <input type="checkbox"  id="terms" />
-                <label htmlFor="terms">Terms and Conditions</label>
-                <input type="submit" value="Create an account" />
-            </form>
-        </div>
-    );
-}
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [termsAccepted, setTermsAccepted] = useState(false);
+
+  const handleSubmit = (e) => {
+
+    e.preventDefault()
+    
+    if (password !== confirmPassword) {
+      alert("Passwords don't match");
+      return;
+    }
+
+    else {
+        alert("Passwords match");
+    }
+  };
+
+  return (
+    <div className="signComponents">
+      <img src={loginImage} alt="login-image" />
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Full Name"
+          value={fullName}
+          onChange={(e) => setFullName(e.target.value)}
+        />
+        <input
+          type="email"
+          placeholder="your email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          name=""
+          id=""
+          placeholder="create password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <input
+          type="password"
+          name=""
+          id=""
+          placeholder="confirm password"
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+        />
+        <input
+          type="checkbox"
+          id="terms"
+          checked={termsAccepted}
+          onChange={() => setTermsAccepted(!termsAccepted)}
+        />
+        <label htmlFor="terms">Terms and Conditions</label>
+        <input type="submit" value="Create an account" />
+      </form>
+    </div>
+  );
+};
 
 export default SignUp;
