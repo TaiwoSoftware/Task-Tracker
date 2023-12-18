68db98e3072955d5a2bbdb794b83892b9e3a0410
Taiwo AKerele <davcodes24@gmail.com>
2023-12-18 10:20:00 +0100

    working on the dashboard

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React from "react";
 
 const Dashboard = () => {
-    return (
-        <div>
-            Dashboard
-        </div>
-    );
-}
+
+    const handleClick = () => {
+        alert('click');
+    }
+
+  return (
+    <div>
+      <div className="addTask">
+        <button onClick={handleClick}>+</button>
+      </div>
+      <form>
+        <input type="text" placeholder="add your task"/>
+        <label htmlFor="start">Deadline to the task</label>
+        <input id="start" type="date" name="" placeholder="date of your task" />
+
+        <input type="radio" name="taskDuration" id="aboutToStart" />
+
+        <input type="radio" name="taskDuration" id="inProgress" />
+
+        <input type="radio" name="taskDuration" id="" />
+      </form>
+    </div>
+  );
+};
 
 export default Dashboard;
