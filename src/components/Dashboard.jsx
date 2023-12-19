9483539fa0ffb2a24b7f98875d468151ee1323a2
Taiwo AKerele <davcodes24@gmail.com>
2023-12-19 15:27:29 +0100

    working on the dashboard

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -15,9 +15,10 @@ const Dashboard = () => {
         <input type="text" placeholder="add your task"/>
         <label htmlFor="start">Deadline to the task</label>
         <input id="start" type="date" name="" placeholder="date of your task" />
-
+        <label htmlFor="aboutToStart">Just started</label>
         <input type="radio" name="taskDuration" id="aboutToStart" />
 
+        <label htmlFor="inProgress"></label>
         <input type="radio" name="taskDuration" id="inProgress" />
 
         <input type="radio" name="taskDuration" id="" />
