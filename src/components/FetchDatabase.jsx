b2fdb1bb4aab106ee70b3e4dc9a26186f6bf7d41
Taiwo AKerele <davcodes24@gmail.com>
2023-12-23 19:21:38 +0100

    fetching the api details

---
+++ src/components/FetchDatabase.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const FetchDatabase = () => {
+    const api = 'https://run.mocky.io/v3/77d5aebf-9d50-4be6-a3df-e8e279677ae9';
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default FetchDatabase;
