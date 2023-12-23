9c2fdab04c664ff89a3eedd389eb9565f335cdfb
Taiwo AKerele <davcodes24@gmail.com>
2023-12-23 19:18:15 +0100

    fill up the form details

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -53,7 +53,46 @@ const SignUp = () => {
       <div className="signComponents">
         <img src={loginImage} alt="login-image" />
         <form onSubmit={handleSubmit}>
-          {/* ... (your existing input fields) */}
+          <input
+            type="text"
+            required
+            placeholder="Full Name"
+            value={fullName}
+            onChange={(e) => setFullName(e.target.value)}
+          />
+          <input
+            type="email"
+            required
+            placeholder="your email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            required
+            name=""
+            id=""
+            placeholder="create password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <input
+            type="password"
+            required
+            name=""
+            id=""
+            placeholder="confirm password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+          <input
+            type="checkbox"
+            required
+            id="terms"
+            checked={termsAccepted}
+            onChange={() => setTermsAccepted(!termsAccepted)}
+          />
+          <label htmlFor="terms">Terms and Conditions</label>
           <input type="submit" value="Create an account" />
           <p className="already">
             Already have an account <span onClick={handleLogin}>Login?</span>
